cccd3260021703e3c2c2ab90a62dfe9a233cbd53
Szikes Adam <SzikesAdam@MacBook>
2022-06-01 20:35:22 +0200

    First version of gpio

--- gpio.h
+++ gpio.h
@@ -3,17 +3,45 @@
 
 #include <avr/io.h>
 
-static inline void gpio_init(void) {
+inline void gpio_init(void) {
   DDRB = (0x7 << 1);
+  DDRB |= (1 << PB3) | (1 << PB5); // MOSI, SCK
 }
 
-static inline void gpio_set_led_red(void) {
+inline void gpio_set_led_red(void) {
   PORTB |= (1 << PORTB1);
 }
 
-static inline void gpio_reset_led_red(void) {
+inline void gpio_reset_led_red(void) {
   PORTB &= ~(1 << PORTB1);
 }
 
+inline void gpio_set_led_green(void) {
+  PORTB |= (1 << PORTB3);
+}
+
+inline void gpio_reset_led_green(void) {
+  PORTB &= ~(1 << PORTB3);
+}
+
+inline void gpio_set_led_blue(void) {}
+
+inline void gpio_reset_led_blue(void) {}
+
+inline void gpio_latch_enable(void) {
+// generate an impulse like this. Turn on the pin then turn it off
+}
+
+inline void gpio_set_blanking(void) {
+// turn it on
+}
+
+inline void gpio_reset_blanking(void) {
+// turn it off
+}
+
+inline void gpio_esp_reset(void) {
+// turn it off then turn it on
+}
 
 #endif // GPIO_H_
